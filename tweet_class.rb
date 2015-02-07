f58ff66c5efdf06e17438cbbde9a4bc9e20f86f5
Michael Cleyn <michael.cleyn@shopify.com>
2015-02-07 13:56:30 -0500

    tweet method fixes

--- tweet_class.rb
+++ tweet_class.rb
@@ -4,14 +4,9 @@ class Tweet
   def valid?
     "#{content}".chars.length <= "#{140}".to_i
   end
-<<<<<<< HEAD
 
   def post
       Tweet.new.valid? == true
       puts "#{content}"
   end
 end
-
-=======
-end
->>>>>>> 7fa5b947908b063640bc74c95b514329d309d3ed
